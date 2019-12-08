b4eb71d9dcd46fb1c00c0d48b8459bd44e144467
cook-a-doodle-do <densukenul@gmail.com>
2019-12-08 12:25:08 +0900

    update

--- raw_device/tap_linux.go
+++ raw_device/tap_linux.go
@@ -1,7 +1,12 @@
 package raw_device
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strings"
+	"syscall"
+	"unsafe"
 )
 
 const (
@@ -15,14 +20,42 @@ type TapLinux struct {
 	file *os.File
 }
 
-func NewTapLinux() (*TapLinux, error) {
-	fd, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
+type ttIF struct {
+	Name  [0x10]byte
+	Flags uint16
+	pad   [0x28 - 0x10 - 2]byte
+}
+
+func NewTapLinux(name string) (*TapLinux, error) {
+	f, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
 	if err != nil {
 		return nil, err
 	}
+	var req ttIF
+	copy(req.Name[:], name)
+	req.Flags = cIFF_NO_PI | cIFF_TAP
+
+	_, _, errno := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		f.Fd(),
+		uintptr(syscall.TUNSETIFF),
+		uintptr(unsafe.Pointer(&req)))
+	if errno != 0 {
+		var msg string
+		switch errno {
+		case 1:
+			msg = fmt.Sprintf("ioctl: requires root privileges")
+		default:
+			msg = fmt.Sprintf("ioctl: %d", errno)
+		}
+		return nil, errors.New(msg)
+	}
+	n := strings.Trim(string(req.Name[:]), "\x00")
+	return &TapLinux{name: n, file: f}, nil
 }
 
-func (t *TapLinux) Close() {
+func (t *TapLinux) Close() error {
+	return t.file.Close()
 }
 
 func (t *TapLinux) Rx() {
--- raw_device/tap_linux_test.go
+++ raw_device/tap_linux_test.go
@@ -1,7 +1,16 @@
 package raw_device
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestHelloWorld2(t *testing.T) {
 	// t.Fatal("not implemented")
+	tap, err := NewTapLinux("tap%d")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	time.Sleep(time.Second * 30)
+	tap.Close()
 }
